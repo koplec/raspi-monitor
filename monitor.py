e0df03ac4a784cb1858ff25c6972501969d6b0f1
fumufumi@gmail.com <fumufumi@gmail.com>
2021-10-31 13:26:25 +0000

    gasに投げるところができるところまで確認

--- monitor.py
+++ monitor.py
@@ -1,14 +1,41 @@
 import os
 import subprocess 
+import datetime
+import json
+import requests
 
-res = subprocess.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], stdout=subprocess.PIPE)
-print(res)
-if res.returncode == 0:
-    print("OK")
-    str = res.stdout.decode() #byte列 -> 文字列
-    temp = int(str)/1000 #文字列 -> int -> div by 1000で温度に変換
-    
-else:
-    print("NG")
-    os.Exit(1)
+from settings import *
 
+
+def main():
+    res = subprocess.run(["cat", "/sys/class/thermal/thermal_zone0/temp"], stdout=subprocess.PIPE)
+    print(res)
+    if res.returncode == 0:
+        print("OK")
+        str = res.stdout.decode() #byte列 -> 文字列
+        temp = int(str)/1000 #文字列 -> int -> div by 1000で温度に変換
+
+        now = datetime.datetime.now()
+        time_stamp = now.strftime("%Y/%m/%d %H:%M:%S.%f")
+
+
+        data = {
+            "time-stamp": time_stamp, "cpu-temp": temp
+        }
+        gas_post(data)
+    else:
+        print("NG")
+        os.Exit(1)
+
+def gas_post(data):
+    try:
+        requests.post(GAS_URL, data=json.dumps(data), headers={"Content-Type": "application/json"})
+    except KeyboardInterrupt:
+        sys.exit()
+    except requests.exceptions.ConnectionError:
+        # 時々このエラーが発生するため追加
+        print("Error, 送信失敗, %s" % data["time_stamp"])
+
+
+if __name__ == "__main__":
+    main()
